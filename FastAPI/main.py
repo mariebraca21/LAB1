00b7d2ae60ee3b398fbecc9e267b0f3e7b00a55f
marierlly <marielabracamonte83@gmail.com>
2024-01-10 08:05:20 -0300

    mi primer proyecto lab1

---
+++ FastAPI/main.py
@@ -0,0 +1,107 @@
+import pandas as pd
+from fastapi import FastAPI
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics.pairwise import linear_kernel
+from sklearn.feature_extraction.text import TfidfVectorizer
+app = FastAPI()
+
+df = pd.read_csv('Df_final.csv')
+
+
+
+#http://127.0.0.1:8000
+@app.get('/')
+def index():
+    return {'mensaje': 'Hola, Pythonianos'}
+
+
+@app.get('/PlayTimeGenre/')
+def PlayTimeGenre(genre: str) -> dict:
+    genre = genre.capitalize()
+    genre_df = df[df[genre] == 1]
+    year_playtime_df = genre_df.groupby(
+        'year')['playtime_forever'].sum().reset_index()
+    max_playtime_year = year_playtime_df.loc[year_playtime_df['playtime_forever'].idxmax(
+    ), 'year']
+    return {"Género": genre, "Año de lanzamiento con más horas jugadas para Género :": int(max_playtime_year)}
+
+@app.get('/UserForGenre/')
+def UserForGenre(genre: str) -> dict:
+    genre = genre.capitalize()
+    genre_df = df[df[genre] == 1]
+    max_playtime_user = genre_df.loc[genre_df['playtime_forever'].idxmax(
+    ), 'user_id']
+    year_playtime_df = genre_df.groupby(
+        'year')['playtime_forever'].sum().reset_index()
+    playtime_list = year_playtime_df.to_dict(orient='records')
+    result = {
+        "Usuario con más horas jugadas para Género " + genre: max_playtime_user,
+        "Horas jugadas": playtime_list}
+    return result
+
+@app.get('/UsersRecommend/')
+def UsersRecommend(year: int) -> dict:
+    df_filtrado = df[(df['year'] == year) & (
+        df['recommend'] == True) & (df['sentiment_score'] == 0)]
+    if df_filtrado.empty:
+        return {"error": 'Valor no encontrado'}
+    df_ordenado = df_filtrado.sort_values(
+        by='sentiment_score', ascending=False)
+    top_3_reseñas = df_ordenado.head(3)
+    resultado = {
+        "Puesto 1": top_3_reseñas.iloc[0]['title'],
+        "Puesto 2": top_3_reseñas.iloc[1]['title'],
+        "Puesto 3": top_3_reseñas.iloc[2]['title']
+    }
+    return resultado
+
+@app.get('/UsersNotRecommed/')
+def UsersRecommend(year: int) -> dict:
+    df_filtrado = df[(df['year'] == year) & (
+        df['recommend'] == False) & (df['sentiment_score'] <= 2)]
+    if df_filtrado.empty:
+        return {"error": 'Valor no encontrado'}
+    df_ordenado = df_filtrado.sort_values(
+        by='sentiment_score', ascending=False)
+    top_3_reseñas = df_ordenado.head(3)
+    resultado = {
+        "Puesto 1": top_3_reseñas.iloc[0]['title'],
+        "Puesto 2": top_3_reseñas.iloc[1]['title'],
+        "Puesto 3": top_3_reseñas.iloc[2]['title']
+    }
+    return resultado
+
+@app.get('/sentiment_analysis/')
+def sentiment_analysis(year: int) -> dict:
+    filtered_df = df[df['year'] == year]
+    sentiment_counts = filtered_df['sentiment_score'].value_counts()
+    result = {
+        "Positive": int(sentiment_counts.get(0, 0)),
+        "Neutral": int(sentiment_counts.get(1, 0)),
+        "Negative": int(sentiment_counts.get(2, 0))
+    }
+    return result
+
+@app.get('/sentiment_analysis/')
+def sentiment_analysis(year: int) -> dict:
+    filtered_df = df[df['year'] == year]
+    sentiment_counts = filtered_df['sentiment_score'].value_counts()
+    result = {
+        "Positive": int(sentiment_counts.get(0, 0)),
+        "Neutral": int(sentiment_counts.get(1, 0)),
+        "Negative": int(sentiment_counts.get(2, 0))
+    }
+    return result
+
+
+@app.get('/recomendacion_juego/{id_juego}')
+def recomendacion_juego(id_juego: int):
+    if id_juego not in muestra['id'].values:
+        return {'mensaje': 'No existe el id del juego.'}
+    titulo = muestra.loc[muestra['id'] == id_juego, 'title'].iloc[0]
+    idx = muestra[muestra['title'] == titulo].index[0]
+    sim_cosine = list(enumerate(cosine_similarity[idx]))
+    sim_scores = sorted(sim_cosine, key=lambda x: x[1], reverse=True)
+    sim_ind = [i for i, _ in sim_scores[1:6]]
+    sim_juegos = muestra['title'].iloc[sim_ind].values.tolist()
+    return {'juegos recomendados': list(sim_juegos)}
